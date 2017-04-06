71695cfe07381459748184977545681712603821
sadooghi <gh.sadooghi@gmail.com>
2017-04-06 18:34:41 -0400

    users have color now

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -15,11 +15,17 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
+let randomColor = () => {
+  const colors = ['#E6E6FA', '#D8BFD8', '#DDA0DD', '#EE82EE', '#FF00FF', '#FF00FF', '#BA55D3', '#9932CC', '#9400D3', '#8A2BE2', '#8B008B', '#800080', '#9370DB', '#7B68EE', '#6A5ACD', '#483D8B', '#663399', '#4B0082'];
+  return colors[Math.floor(Math.random()*colors.length)];
+}
+let userColors = {};
+
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 
-wss.broadcast = function broadcast(data) {
+wss.broadcast = function (data) {
   wss.clients.forEach(function each(client) {
     if (client.readyState === WebSocket.OPEN) {
       client.send(JSON.stringify(data));
@@ -41,17 +47,21 @@ wss.on('connection', (ws) => {
     switch(data.type) {
       case "postMessage":
         data.type = "incomingMessage";
+        data.color =  userColors[data.username];
         console.log(data);
         wss.broadcast(data);
+
         break;
       case "postNotification":
         data.type = "incomingNotification";
         console.log(data);
         wss.broadcast(data);
+        userColors[data.currentName] = randomColor();
+
         break;
     }
 
-    });
+  });
 
   ws.on('close', () => {
      console.log('Client disconnected')
@@ -61,5 +71,5 @@ wss.on('connection', (ws) => {
      // of the client who disconnected
    });
 
-  });
+});
 
